e3c7a1e6fde07de840374ed87ff1f5fc16d46b0f
starchyunderscore <spenceramay22@gmail.com>
2023-11-06 10:44:55 -0500

    Update codeHScheat.user.js
    Make the code easier to understand by adding comments and making the console logs actually say what's going on instead of things like "HI" :thumbsup:

--- codeHScheat.user.js
+++ codeHScheat.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name        New script - codehs.com
-// @namespace   Violentmonkey Scripts
+// @name        CodeHScheat
+// @namespace   https://github.com/starchyunderscore
 // @match       https://codehs.com/student/*
 // @grant       none
-// @version     1.0
-// @author      -
-// @description 11/2/2023, 10:04:43 AM
+// @version     2.0
+// @author      Starchyunderscore
+// @description Script to automatically cheat on codehs.com
 // ==/UserScript==
 
-const EVENT_OPTIONS = {bubbles: true, cancelable: false, composed: true};
+const EVENT_OPTIONS = {bubbles: true, cancelable: false, composed: true}; // Constants for faking typing input
 const EVENTS = {
   BLUR: new Event("blur", EVENT_OPTIONS),
   CHANGE: new Event("change", EVENT_OPTIONS),
   INPUT: new Event("input", EVENT_OPTIONS),
 };
 
-function toHex(s) {
+function toHex(s) { // Hex encode strings
   var s = unescape(encodeURIComponent(s))
   var h = ''
   for (var i = 0; i < s.length; i++) {
@@ -24,14 +24,14 @@ function toHex(s) {
   return h
 }
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+const delay = ms => new Promise(res => setTimeout(res, ms)); // Easy delays
 
-async function doAnswer(id, inputElement, BUTTON_ONE) {
+async function doAnswer(id, inputElement, BUTTON_ONE) { // Answers exercises
   document.getElementsByClassName("ace_scroller")[0].click() // Select all so that the current answer is deleted
   inputElement.select();
   var text = await fetch("https://raw.githubusercontent.com/starchyunderscore/codehscheat/main/answers/"+id+".txt") // Fetch the answer, then convert it to text
   .then(response => response.text());
-  if(text == "404: Not Found") {
+  if(text == "404: Not Found") { // If there isn't an answer yet, tell the user and stop
     alert("There is not an answer for this assignment yet");
   } else {
     inputElement.value = text; // Simulate typing the text
@@ -40,55 +40,57 @@ async function doAnswer(id, inputElement, BUTTON_ONE) {
     inputElement.dispatchEvent(EVENTS.INPUT);
     inputElement.dispatchEvent(EVENTS.BLUR);
     BUTTON_ONE.click(); // Click the submit button
-    await delay(10000); // Wait ten seconds before trying to click the second submit button to give the autograder time
+    await delay(15000); // Wait fifteen seconds before trying to click the second submit button to give the autograder time
     document.getElementById("submit-correct").click();
   }
 }
-async function answerQuiz(questions, id) {
-  var questions = document.querySelector(".quiz-questions").querySelectorAll("ol");
+
+async function answerQuiz(questions, id) { // Answer the quizzes
+  var questions = document.querySelector(".quiz-questions").querySelectorAll("ol"); // Get each of the questions
   var text = await fetch("https://raw.githubusercontent.com/starchyunderscore/codehscheat/main/answers/"+id+".txt") // Fetch the answer, then convert it to text
   .then(response => response.text());
-  if(text == "404: Not Found") {
+  if(text == "404: Not Found") { // Tell the user and stop if there is no answer yet
     alert("There is not an answer for this assignment yet");
   } else {
     var answers = text.split("");
-    answers.remove(-1);
-    for(i=0;i<answers.length;i++) {
-      var choices = questions[i].querySelectorAll('input');
-      choices[answers[i]].click();
+    answers.remove(-1); // Remove the trailing newline
+    for(i=0;i<answers.length;i++) { // Repeat for as many questions as there are
+      var choices = questions[i].querySelectorAll('input'); // Select the answers in each question
+      choices[answers[i]].click(); // Click the correct answer
     }
-    document.getElementById("next-button").click();
-    await delay(1000);
-    document.querySelector("#alert-modal a.btn.btn-main.alert-close.ok-button").click();
+    document.getElementById("next-button").click(); // Click the submit button
+    await delay(1000); // Wait one second for the "are you sure" dialouge to appear
+    document.querySelector("#alert-modal a.btn.btn-main.alert-close.ok-button").click(); // Click the second submit button
   }
 }
 
-async function skipExample(BUTTON_ONE) {
-  await delay(10000);
+async function skipExample(BUTTON_ONE) { // Skips the examples
+  await delay(10000); // Wait a bit, then click "Next"
   BUTTON_ONE.click();
 }
 
-async function skipVideo(BUTTON_TWO) {
-  document.getElementById("play-btn-container").children[0].click();
-  await delay(10000)
-  BUTTON_TWO.click();
+async function skipVideo(BUTTON_TWO) { // Skip the videos
+  document.getElementById("play-btn-container").children[0].click(); // Click the start viedo button
+  await delay(10000) // Wait a bit
+  BUTTON_TWO.click(); // Click the next button
 }
 
-window.addEventListener("load", (event) => {
+window.addEventListener("load", (event) => { // Start the script only after the page has fully loaded
+  console.log("START");
   try { // "Submit + Continue" or "Next" button.
     var BUTTON_ONE = document.getElementsByClassName("__abacus_button")[2];
   } catch {
-    console.log("HI")
+    console.log("BUTTON_ONE failed")
   }
   try { // Done button on video pages
     var BUTTON_TWO = document.getElementById("done-button");
   } catch {
-    console.log("HI")
+    console.log("BUTTON_TWO failed")
   }
   try { // Quiz name
     var QUIZ_NAME = document.getElementsByClassName("center")[3].innerHTML;
   } catch {
-    console.log("HI")
+    console.log("QUIZ_NAME failed")
   }
   try {
     if(BUTTON_ONE.innerHTML == "Next") { // skip if it is an example
@@ -101,28 +103,27 @@ window.addEventListener("load", (event) => {
       doAnswer(assignment,inputElement,BUTTON_ONE);
     }
   } catch {
-    console.log("move on")
+    console.log("NOT EXAMPLE OR EXERCISE")
   } finally {
     try {
       if (BUTTON_TWO != undefined) { // Skip the videos
         skipVideo(BUTTON_TWO);
       }
     } catch {
-      console.log("move on")
+      console.log("NOT VIEDO")
     } finally {
       try {
         if (QUIZ_NAME[0] != "\n") { // Answer the quizzes
-          if (document.querySelector(".btn.btn-lg.continue-btn").innerHTML != "Submit") {
+          if (document.querySelector(".btn.btn-lg.continue-btn").innerHTML != "Submit") { // If the quiz is already submitted, move on to the next assignment
             document.querySelector(".btn.btn-lg.continue-btn").click();
-          } else {
-            document.querySelector(".btn.btn-lg.continue-btn").click();
+          } else { // If the quiz is not already submitted, answer the quiz
             const questions = document.querySelector(".quiz-questions").querySelectorAll("li");
             const id = toHex(QUIZ_NAME)
             answerQuiz(questions, id)
           }
         }
       } catch {
-        console.log("move on")
+        console.log("NOT QUIZ")
       } finally {
         console.log("DONE")
       }
